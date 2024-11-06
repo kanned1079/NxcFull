4a0d6c86143bf2a45573f18407997a963da4f868
kanned1079 <kanned1079@gmail.com>
2024-11-06 12:00:13 +0800

    update1106

---
+++ book-mgr/src/views/Login.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+
+</script>
+
+<script lang="ts">
+export default {
+  name: 'Login'
+}
+</script>
+
+<template>
+<div class="root"></div>
+</template>
+
+<style scoped lang="less">
+.root {
+
+}
+</style>
