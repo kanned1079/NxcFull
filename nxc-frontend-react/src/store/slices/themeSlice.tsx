52e1403c45096d7da97eb550a5c2edebf7a5b057
kanna <kanna@kamiizumi.onmicrosoft.com>
2025-01-04 19:21:06 +0800

    update0104

--- nxc-frontend-react/src/store/slices/themeSlice.tsx
+++ nxc-frontend-react/src/store/slices/themeSlice.tsx
@@ -10,6 +10,18 @@ interface ThemeState {
 const useThemeSlice: StateCreator<ThemeState, [], [], ThemeState> = (set) => ({
     theme: 'light',
     glacierBlue: {  // 主题配置 冰川蓝
+        theme: {
+            token: {
+              colorPrimary: ''
+            },
+            components: {
+                Button: {
+                },
+                Drawer: {
+
+                }
+            },
+        }
     },
     toggleTheme: () => set((state: ThemeState) => ({
         theme: state.theme = 'dark'
